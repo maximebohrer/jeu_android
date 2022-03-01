75377300438cac43cd88cd05a9bc0d3c89399297
maximebohrer <maxime21.bohrer@gmail.com>
2022-03-01 10:16:19 +0100

    ok

--- app/src/main/java/com/example/jeuandroid/MainActivity.java
+++ app/src/main/java/com/example/jeuandroid/MainActivity.java
@@ -2,13 +2,100 @@ package com.example.jeuandroid;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
+    float xStart = 0;
+    float yStart = 0;
+
+    float xCoeff = 0;
+    float yCoeff = 0;
+
+    float xPadOffset = 0;
+    float yPadOffset = 0;
+    float padOffsetFactor = 100;
+
+    View mainLayout;
+    ImageView padInt;
+    ImageView padExt;
+    ImageView tieImage;
+    Tie tie;
+
+    @SuppressLint("ClickableViewAccessibility")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Objets du layout
+        mainLayout = findViewById(R.id.mainLayout);
+        padInt = (ImageView) findViewById(R.id.padInt);
+        padExt = (ImageView) findViewById(R.id.padExt);
+        tieImage = (ImageView) findViewById(R.id.tie);
+
+        tie = new Tie(tieImage);
+
+        //Layout listener pour placer le joystick
+        mainLayout.getViewTreeObserver().addOnGlobalLayoutListener(() -> {
+            positionPadExt();
+            positionPadInt();
+            tie.setMaxScreenXY(mainLayout.getWidth(), mainLayout.getHeight());
+            tie.setImageWH();
+        });
+
+        //Touch listener pour gérer le joystick
+        padInt.setOnTouchListener((v, event) -> {
+            switch (event.getAction()){
+                case MotionEvent.ACTION_DOWN:
+                    xStart = event.getX();
+                    yStart = event.getY();
+                    break;
+                case MotionEvent.ACTION_MOVE:
+                    float xMoved = event.getX() - xStart;
+                    float yMoved = event.getY() - yStart;
+                    xCoeff = xMoved / 150f;
+                    yCoeff = yMoved / 150f;
+                    float squaredLength = xCoeff * xCoeff + yCoeff * yCoeff;
+                    if (squaredLength > 1) {
+                        xCoeff /= Math.sqrt(squaredLength);
+                        yCoeff /= Math.sqrt(squaredLength);
+                    }
+                    xPadOffset = padOffsetFactor * xCoeff;
+                    yPadOffset = padOffsetFactor * yCoeff;
+                    positionPadInt();
+                    tie.setSpeed(xCoeff, yCoeff);
+                    break;
+                case MotionEvent.ACTION_UP:
+                    xCoeff = 0;
+                    yCoeff = 0;
+                    xPadOffset = 0;
+                    yPadOffset = 0;
+                    positionPadInt();
+                    tie.setSpeed(0,0);
+                    break;
+                default:
+                    return false;
+            }
+            return true;
+        });
+
+        tie.startMove();
     }
+
+    void positionPadExt() {
+        padExt.setX(mainLayout.getWidth() / 2f - padExt.getWidth() / 2f);
+        padExt.setY(mainLayout.getHeight() - padExt.getHeight() - 50);
+    }
+
+    void positionPadInt() {
+        padInt.setX(padExt.getX() + (padExt.getWidth() - padInt.getWidth()) / 2f + xPadOffset);
+        padInt.setY(padExt.getY() + (padExt.getHeight() - padInt.getHeight()) / 2f + yPadOffset);
+    }
+
+
 }
---
+++ app/src/main/java/com/example/jeuandroid/Tie.java
@@ -0,0 +1,76 @@
+package com.example.jeuandroid;
+
+import android.os.Handler;
+import android.util.Log;
+import android.widget.ImageView;
+import java.lang.Math;
+
+public class Tie {
+    final int delay = 10;
+    ImageView tieImage;
+    Handler handlerForMovingTie;
+    Runnable movingTie;
+    float x, y, angle, V, newV, xMax, yMax, w, h;
+    final float frottements = 0.002f;
+
+
+    public Tie(ImageView tieImage){
+        this.tieImage = tieImage;
+        this.w = tieImage.getWidth();
+        this.h = tieImage.getHeight();
+    }
+
+    public void startMove(){
+        x = tieImage.getX();
+        y = tieImage.getY();
+
+        handlerForMovingTie = new Handler();
+        movingTie = new Runnable() {
+            @Override
+            public void run() {
+                update();
+                handlerForMovingTie.postDelayed(this, delay);
+            }
+        };
+        movingTie.run();
+    }
+
+    private void update() {
+        if (newV >= V) V = newV;
+        else V -= frottements * delay;
+        if (V < 0) V = 0;
+
+
+        //V = newV;
+        x += V * delay * Math.cos(angle);
+        y += V * delay * Math.sin(angle);
+
+        if (x > xMax - w / 2) x = xMax - w / 2;
+        if (x < - w / 2) x = - w / 2;
+        if (y > yMax - w / 2) y = yMax - w / 2;
+        if (y < - w / 2) y = - w / 2;
+
+        tieImage.setRotation((float)Math.toDegrees(angle + Math.PI / 2));
+        tieImage.setX(x);
+        tieImage.setY(y);
+    }
+
+    public void setSpeed(float xCoeff, float yCoeff) {
+        newV = (float) Math.sqrt(xCoeff * xCoeff + yCoeff * yCoeff) * 1;
+        if (!(xCoeff == 0) && !(yCoeff == 0)) {
+            if (xCoeff == 0) angle = (float)(yCoeff > 0 ? Math.PI / 2 : - Math.PI / 2);
+            else if (yCoeff == 0) angle = (float)(xCoeff > 0 ? 0 : Math.PI);
+            else angle = (float)(xCoeff > 0 ? Math.atan(yCoeff / xCoeff) : Math.atan(yCoeff / xCoeff) + Math.PI);
+        }
+    }
+
+    public void setMaxScreenXY(float xMax, float yMax) {
+        this.xMax = xMax;
+        this.yMax = yMax;
+    }
+
+    public void setImageWH() {
+        w = tieImage.getWidth();
+        h = tieImage.getHeight();
+    }
+}
