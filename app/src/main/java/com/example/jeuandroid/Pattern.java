26376f6f38ddbbc3116919d9369bfc3062ab3f19
Alin236 <nilavan.deva@etu.imt-lille-douai.fr>
2022-03-01 13:02:50 +0100

    Modification des classes asteroid et pattern

--- app/src/main/java/com/example/jeuandroid/Pattern.java
+++ app/src/main/java/com/example/jeuandroid/Pattern.java
@@ -30,19 +30,8 @@ public class Pattern {
     }
 
     static Path getElipse(){
-        float n= 400f;
-        Random random = new Random();
         Path path = new Path();
-
-        Path path2 = new Path();
-        path2.arcTo(0f, -n, 800f, 0*n, -90f, -180f, true);
-        path2.arcTo(0f, 0*n, 800f, n, -90f, 180f, true);
-
-        path.addPath(path2);
-        path.addPath(path2, 0,2*n);
-        path.addPath(path2, 0,4*n);
-        path.addPath(path2, 0,6*n);
-
+        path.arcTo(0f, 0, 800f, 800f, -90f, 359f, true);
         return path;
     }
 
