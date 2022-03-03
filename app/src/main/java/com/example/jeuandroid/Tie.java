66ac4b6e7cc7dd32c86aa7669f2373d8959eddae
Alin236 <nilavan.deva@etu.imt-lille-douai.fr>
2022-03-03 12:48:55 +0100

    Tie : Collision + hitBox circulaire

--- app/src/main/java/com/example/jeuandroid/Tie.java
+++ app/src/main/java/com/example/jeuandroid/Tie.java
@@ -1,5 +1,6 @@
 package com.example.jeuandroid;
 
+import android.graphics.Path;
 import android.os.Handler;
 import android.util.Log;
 import android.widget.ImageView;
@@ -9,9 +10,12 @@ public class Tie {
     final int delay = 10;
     ImageView tieImage;
     Handler handlerForMovingTie;
+    Handler handlerForCollisionTie;
     Runnable movingTie;
+    Runnable collisionTie;
     float x, y, angle, V, newV, xMax, yMax, w, h;
     final float frottements = 0.002f;
+    Asteroid[] asteroids = new Asteroid[0];
 
     public Tie(ImageView tieImage){
         this.tieImage = tieImage;
@@ -71,5 +75,51 @@ public class Tie {
     public void setImageWH() {
         w = tieImage.getWidth();
         h = tieImage.getHeight();
+    }
+
+    public Path getHitBox(){
+        Path path = new Path();
+        float x = tieImage.getX();
+        float y = tieImage.getY();
+        float w = tieImage.getMeasuredWidth();
+        float h = tieImage.getMeasuredHeight();
+        path.arcTo(x,y,x+w,y+h,0f, 359f, true);
+        return path;
+    }
+
+    public void startCollision(){
+        handlerForCollisionTie = new Handler();
+        collisionTie = new Runnable() {
+            @Override
+            public void run() {
+                if(isColliding()){
+                    explode();
+                };
+                handlerForMovingTie.postDelayed(this, delay);
+            }
+        };
+        collisionTie.run();
+    }
+
+    public boolean isColliding(){
+        Path path = new Path();
+        Path hitBoxTie = getHitBox();
+        Asteroid asteroid;
+        for(int i = 0; i<asteroids.length; i++){
+            asteroid = asteroids[i];
+            path.op(hitBoxTie, asteroid.getHitBox(), Path.Op.INTERSECT);
+            if(!path.isEmpty()){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void explode(){
+        Log.d("Explode", "Le vaisseau est entré en collision");
+    }
+
+    public void setAsteroids(Asteroid[] asteroids){
+        this.asteroids = asteroids;
     }
 }
